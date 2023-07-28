dbc6e9d7e9f55368630801a4231ffab7363e454b
cgathua <114153640+cgathua@users.noreply.github.com>
2023-07-28 09:43:32 -0400

    edit: files

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,52 +18,50 @@ const NavigationMenuDesktop = () => {
     ]
     return (
         <header>
-            <nav>
-                <a href='#' className='logo'>Logo</a>
-                <NavigationMenu.Root className='NavigationMenuRoot' value='string'>
-                    <NavigationMenu.List className='NavigationMenuList'>
-                        <NavigationMenu.Item>
-                            <NavigationMenu.Link className="NavigationMenuLink" href="#">
-                                Home
-                            </NavigationMenu.Link>
-                        </NavigationMenu.Item>
+            <a href='#' className='logo'>Logo</a>
+            <NavigationMenu.Root className='NavigationMenuRoot' value='string'>
+                <NavigationMenu.List className='NavigationMenuList'>
+                    <NavigationMenu.Item>
+                        <NavigationMenu.Link className="NavigationMenuLink" href="#">
+                            Home
+                        </NavigationMenu.Link>
+                    </NavigationMenu.Item>
 
-                        <NavigationMenu.Item>
-                            <NavigationMenu.Link className="NavigationMenuLink" href="#phones">
-                                Phones
-                            </NavigationMenu.Link>
-                        </NavigationMenu.Item>
+                    <NavigationMenu.Item>
+                        <NavigationMenu.Link className="NavigationMenuLink" href="#phones">
+                            Phones
+                        </NavigationMenu.Link>
+                    </NavigationMenu.Item>
 
-                        <NavigationMenu.Item>
-                            <NavigationMenu.Link className="NavigationMenuLink" href="#refurbished">
-                                Refurbished
-                            </NavigationMenu.Link>
-                        </NavigationMenu.Item>
+                    <NavigationMenu.Item>
+                        <NavigationMenu.Link className="NavigationMenuLink" href="#refurbished">
+                            Refurbished
+                        </NavigationMenu.Link>
+                    </NavigationMenu.Item>
 
-                        <NavigationMenu.Item>
-                            <NavigationMenu.Link className="NavigationMenuLink" href="#faq">
-                                FAQs
-                            </NavigationMenu.Link>
-                        </NavigationMenu.Item>
+                    <NavigationMenu.Item>
+                        <NavigationMenu.Link className="NavigationMenuLink" href="#faq">
+                            FAQs
+                        </NavigationMenu.Link>
+                    </NavigationMenu.Item>
 
-                        <NavigationMenu.Item>
-                            <NavigationMenu.Link className="NavigationMenuLink" href="#contact">
-                                Contact
-                            </NavigationMenu.Link>
-                        </NavigationMenu.Item>
+                    <NavigationMenu.Item>
+                        <NavigationMenu.Link className="NavigationMenuLink" href="#contact">
+                            Contact
+                        </NavigationMenu.Link>
+                    </NavigationMenu.Item>
 
-                        <NavigationMenu.Item>
-                            <NavigationMenu.Link className="NavigationMenuLink" href="https://github.com/cgathua?tab=repositories" target='blank'>
-                                Github
-                            </NavigationMenu.Link>
-                        </NavigationMenu.Item>
-                    </NavigationMenu.List>
-                </NavigationMenu.Root>
-                <ul className={`NavBar2`}>
-                    {ulLists.map(elem => <li key={elem.id}><a aria-label="mobile nav" href={elem.link}>{elem.title}</a></li>)}
-                </ul>
-                <button className="MenuBtn" aria-label="menu" aria-haspopup="true" aria-expanded="false"><HamburgerMenuIcon className="IconButton" /></button>
-            </nav>
+                    <NavigationMenu.Item>
+                        <NavigationMenu.Link className="NavigationMenuLink" href="https://github.com/cgathua?tab=repositories" target='blank'>
+                            Github
+                        </NavigationMenu.Link>
+                    </NavigationMenu.Item>
+                </NavigationMenu.List>
+            </NavigationMenu.Root>
+            <ul className={`NavBar2`}>
+                {ulLists.map(elem => <li key={elem.id}><a aria-label="mobile nav" href={elem.link}>{elem.title}</a></li>)}
+            </ul>
+            <button className="MenuBtn" aria-label="menu" aria-haspopup="true" aria-expanded="false"><HamburgerMenuIcon className="IconButton" /></button>
         </header>
     );
 };
