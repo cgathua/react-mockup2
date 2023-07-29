c57d975e917e648f5faf05b19018628d829d2213
cgathua <114153640+cgathua@users.noreply.github.com>
2023-07-29 13:53:34 -0400

    Create Phones.tsx

---
+++ src/components/Phones.tsx
@@ -0,0 +1,20 @@
+
+function Phones(){
+    return (
+        <section id="phones">
+            <h2>Phones</h2>
+            <div className="container">
+                <article className="ProductsColumn">
+                    <picture>
+                        <source srcSet="" />
+                        <img src="" alt="" />
+                    </picture>
+                    <h3>iphone X</h3>
+                    <button className="btn"></button>
+                </article>
+            </div>
+        </section>
+    );
+};
+
+export default Phones
